1d330c73b9ee585751df7216ea992040a40641f9
Temirlan Tyulyubayev <temikbjj@gmail.com>
2026-09-15 14:48:22 +0800

    Add FileManager utility class for file handling operations

---
+++ src/com/apu/hms/utils/FileManager.java
@@ -0,0 +1,20 @@
+package com.apu.hms.utils;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class FileManager {
+
+    private static final String DATA_FOLDER = "data";
+    private static final String DELIMITER = "|";
+
+    private FileManager() {
+    }
+
+    
+}
